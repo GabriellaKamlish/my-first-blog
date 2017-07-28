f407c359a3affe161a590b00490ad7f2481528d3
gabriellakamlish <gabriellakamlish@hotmail.co.uk>
2017-07-28 11:26:13 +0100

    Added views to create/edit blog post inside the site.

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,8 @@ from .forms import PostForm
 from django.shortcuts import redirect
 
 def post_list(request):
+    user = authenticate(username='gabriellakamlish', password='Gabriella1')
+    login(request, user)
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
